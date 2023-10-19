848d776e20328e496e4e4cfa23860d7bd8450653
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-19 20:06:10 -0300

    refactor(connected): precisamos usar o object

--- src/connected.ts
+++ src/connected.ts
@@ -1,8 +1,9 @@
-const connected = (target: object, propertyKey: PropertyKey) =>
-  Reflect.defineProperty(target, 'connectedCallback', {
+const connected = (target: object, propertyKey: PropertyKey) => {
+  Object.defineProperty(target, 'connectedCallback', {
     value () {
       return this[propertyKey]()
     }
   })
+}
 
 export default connected
