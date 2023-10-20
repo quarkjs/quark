780a949fd215a043cfcfb98b8ba6b6d87130456e
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-19 23:25:52 -0300

    refactor: preciso deixar a sobreescrita do metodo

--- src/connected.ts
+++ src/connected.ts
@@ -1,6 +1,8 @@
 const connected = (target: object, propertyKey: PropertyKey) => {
+  const method = target.connectedCallback
   Object.defineProperty(target, 'connectedCallback', {
     value () {
+      method?.call(this)
       return this[propertyKey]()
     }
   })
--- src/on.ts
+++ src/on.ts
@@ -2,12 +2,17 @@ const on = new Proxy({}, {
   get: (_: object, event: keyof WindowEventMap) =>
     (query: string) =>
       (target: any, propertyKey: PropertyKey) => {
-        const method = target.prototype.connectedCallback
+        const method = target.connectedCallback
         Object.defineProperty(target, 'connectedCallback', {
           value () {
-            this.addEventListener(event, (e) => (e.target.matches(query) && Reflect.apply(this[propertyKey], this, [e])))
+            requestAnimationFrame(() => {
+              this.addEventListener(event, (e) => (
+                e.target.matches(query) && Reflect.apply(this[propertyKey], this, [e])
+              ))
+            })
             return method?.call(this)
-          }
+          },
+          writable: true
         })
       }
 })
